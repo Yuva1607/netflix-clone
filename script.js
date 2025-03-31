f460226f4bbefaf049113560433152c9dd62579d
Yuva1607 <yuvashakkthi@gmail.com>
2025-03-31 22:23:20 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const API_KEY = "ccb73448281e7c34fd6b8eef24ede3c1";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = `${BASE_URL}/movie/popular?api_key=${API_KEY}`;
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 
 async function fetchMovies() {
     try {
         const response = await fetch(API_URL);
         const data = await response.json();
-        console.log(data.results); // Check if movies are being fetched
+        displayMovies(data.results);
     } catch (error) {
         console.error("Error fetching movies:", error);
     }
+}
+
+function displayMovies(movies) {
+    const moviesContainer = document.getElementById("movies");
+    moviesContainer.innerHTML = "";
+
+    movies.forEach(movie => {
+        const movieElement = document.createElement("div");
+        movieElement.classList.add("movie");
+        movieElement.innerHTML = `
+            <img src="${IMG_PATH}${movie.poster_path}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+        `;
+        moviesContainer.appendChild(movieElement);
+    });
 }
 
 fetchMovies();
